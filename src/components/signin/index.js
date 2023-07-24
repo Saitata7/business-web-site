0d520f680651b06f57937a3b2b96e29170a77890
saitata1 <96823996+saitata1@users.noreply.github.com>
2023-07-24 03:08:30 -0400

    emailjs added

--- src/components/signin/index.js
+++ src/components/signin/index.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import emailjs from '@emailjs/browser';
 import {
   SigninContainer,
   SigninWrapper,
@@ -8,20 +9,35 @@ import {
 import { Button } from "../ButtonElements";
 
 const Signin = () => {
+  const form = useRef();
+  const [isMessageSent, setMessageSent] = useState(false);
+
+  const sendEmail = (e) => {
+  e.preventDefault();
+  setMessageSent(true);
+
+    emailjs.sendForm('service_6vy0qjs', 'template_d3qyfun', form.current, 'g-SPDJBPk49brtonK')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+    };
   return (
     <SigninContainer>
       <SigninWrapper className="py-5 px-5">
         <div className="my-auto">
           <h3 className="center-align mb-5">Sign In to your Account</h3>
-          <form>
+          <form ref={form} onSubmit={sendEmail}>
             <SigninP>Email</SigninP>
             <SigninInput type="text" placeholder="Enter your email..." />
             <SigninP>Name</SigninP>
             <SigninInput type="text" placeholder="Enter your Name..." />
             <SigninP>Message</SigninP>
             <SigninInput type="text" placeholder="Enter your Message what you want to say..." />
-            <Button className="mt-5 mb-3">Continue</Button>
+            <Button type="submit" value="Send" className="mt-5 mb-3">Continue</Button>
           </form>
+          {isMessageSent && <p className="center-align">Message has been sent!</p>}
           <p className="center-align">
             Okay, Bye <a href="/">Back</a>
           </p>
