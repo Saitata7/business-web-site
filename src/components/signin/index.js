422f92fd725b72e5d063eac056207d83d48bdcf6
saitata1 <96823996+saitata1@users.noreply.github.com>
2023-07-25 22:56:03 -0400

    emailjs code updated

--- src/components/signin/index.js
+++ src/components/signin/index.js
@@ -6,38 +6,49 @@ import {
   SigninP,
   SigninInput,
 } from "./SigninElements";
-import { Button } from "../ButtonElements";
 
 const Signin = () => {
   const form = useRef();
+  const [emailError, setEmailError] = useState('');
   const [isMessageSent, setMessageSent] = useState(false);
 
   const sendEmail = (e) => {
   e.preventDefault();
-  setMessageSent(true);
-
-    emailjs.sendForm('service_6vy0qjs', 'template_d3qyfun', form.current, 'g-SPDJBPk49brtonK')
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    
+  if (!emailRegex.test(e.target.email.value)) {
+    setEmailError('Please enter a valid email address.');
+    return;
+  }
+  setEmailError('');
+  console.log(form.current);
+    emailjs.sendForm('service_6vy0qjs', 'template_d3qyfun', form.current, 'sxMP4F-UHWtVRUWL6')
       .then((result) => {
           console.log(result.text);
+          setMessageSent(true);
+          e.target.reset();
       }, (error) => {
           console.log(error.text);
+          setMessageSent(false);
+          console.log(form.current);
       });
     };
   return (
     <SigninContainer>
       <SigninWrapper className="py-5 px-5">
         <div className="my-auto">
-          <h3 className="center-align mb-5">Sign In to your Account</h3>
+          <h3 className="center-align mb-5">Sending message to Company</h3>
           <form ref={form} onSubmit={sendEmail}>
+            {emailError && <p style={{ color: 'red' }}>{emailError}</p>}
             <SigninP>Email</SigninP>
-            <SigninInput type="text" placeholder="Enter your email..." />
+            <SigninInput type="text" placeholder="Enter your email..." name="email" required/>
             <SigninP>Name</SigninP>
-            <SigninInput type="text" placeholder="Enter your Name..." />
+            <SigninInput type="text" placeholder="Enter your Name..." name="name"/>
             <SigninP>Message</SigninP>
-            <SigninInput type="text" placeholder="Enter your Message what you want to say..." />
-            <Button type="submit" value="Send" className="mt-5 mb-3">Continue</Button>
+            <SigninInput type="text" placeholder="Enter your Message what you want to say..." name="message"/>
+            <SigninInput type="submit" value="Send" className="mt-5 mb-3" style={{backgroundColor:"#010606",color:"#fff",borderRadius: '50px'}}/>
           </form>
-          {isMessageSent && <p className="center-align">Message has been sent!</p>}
+          {isMessageSent && <p style={{ color: 'green' }} className="center-align">Message has been sent!</p>}
           <p className="center-align">
             Okay, Bye <a href="/">Back</a>
           </p>
